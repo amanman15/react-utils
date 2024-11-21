0d059b8224572e48c4bab7e85c18b3324913e405
Aman Jain <aman.jain04@nagarro.com>
2024-11-21 16:36:43 +0530

    a dark mode enabled for body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import React from 'react'
 import Navbar from './components/Navbar';
+import { useState } from 'react';
 
 function App() {
+  const[mode, myMode]=useState("light")
+  // const changeMode = () => {
+  //   if (mode==="light"){
+  //     myMode("dark")
+  //   }
+  // }
+  document.body.style.backgroundColor="purple"
+  const chnageColor = () => {
+    if (document.body.style.backgroundColor==="purple"){
+        document.body.style.backgroundColor="black"
+    }
+    else{
+      document.body.style.backgroundColor="purple"
+    }
+  }
+
   return (
-    <Navbar/>
+    <>
+    <Navbar background="dark"/>
+    <div className="container my-3">
+    <div className="btn btn-primary" onClick={chnageColor}>Click to change the background color
+    </div>
+    </div>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
+
+    const searchConsle = () => {
+      console.log("You clicked search button")
+      alert("Clikced")
+    }
   return (
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
+<nav className={`navbar navbar-expand-lg bg-${props.background} border-bottom border-body`}  data-bs-theme={props.background}>
   <div className="container-fluid">
     <a className="navbar-brand" href="/">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -16,7 +21,7 @@ export default function Navbar() {
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-success" type="submit" onClick={searchConsle}>Search</button>
       </form>
     </div>
   </div>
