7c30d587bfb25581211f8f9ee15316bad9adc0ab
Aman Jain <aman.jain04@nagarro.com>
2024-11-24 13:28:51 +0530

    added the preview and words count while typing

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -24,15 +24,28 @@ export default function TextArea() {
             let newText=caseText.toLowerCase()
             functionCase(newText)
         }
+    }  
+    // Clear the content
+    const clearText = () => {
+        functionCase("")
     }
-    
+    const trimmedText=caseText.trim()
+      
   return (
+    <>
     <div className="container">
         <div className="form-size">
-        <textarea className="form-control" value={caseText} onChange={handleOnChange} id="floatingTextarea" rows="8" cols="20"></textarea>
+        <textarea className="form-control" value={caseText} onChange={handleOnChange} placeholder='Enter text here' id="floatingTextarea" rows="8" cols="20"></textarea>
         </div>
-        <button className='btn btn-primary my-3' onClick={convertUpper} >{upperCase}</button>
+        <button className='btn btn-primary my-3 mx-3' onClick={convertUpper} >{upperCase}</button>
+        <button className='btn btn-primary my-3' onClick={clearText} >Clear Text</button>
     </div>
+    <div className="container">
+        <h1>Preview</h1>
+        <p>{caseText}</p>
+        <p>Total Number of letters in text is {caseText.length} and total words are {trimmedText === "" ? 0 : trimmedText.split(" ").length} </p>
+    </div>
+    </>
   )
 
 }
